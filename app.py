85876dea60ccc040a6dc1d65576e826018954f7b
Ryan Casey <ryepdx@gmail.com>
2014-05-17 16:56:44 -0700

    Move Python path modification to __init__.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os, sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/lib/python2.7/site-packages")
+# Explicit import to enable 'python app.py'
+import __init__ 
 
 from flask import Flask
 app = Flask(__name__)
